188ef82ec91b5bb471084fa087cea64b1e5382d9
Laura Bigoni <laurabigoni.92@gmail.com>
2022-02-14 12:33:18 +0100

    added components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-	<div id="app"></div>
+	<div id="app">
+		<Header />
+		<main>
+			<Products />
+		</main>
+	</div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+import Products from "./components/Products.vue";
+
 export default {
-	name: "",
-	components: {},
+	name: 'App',
+	components: {
+		Header,
+		Products,
+	},
 };
 </script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+export default {
+	name: "Header",
+};
+</script>
+
+<style>
+</style>
